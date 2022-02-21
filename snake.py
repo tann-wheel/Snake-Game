b84d0047fd06e7c7f544679dab45ec9b3d52325d
tann-wheel <95561506+tann-wheel@users.noreply.github.com>
2022-02-21 19:28:30 +0530

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,65 @@
+from turtle import Turtle
+positions=[(0,0),(-20,0),(-40,0)]
+UP=90
+DOWN=270
+LEFT=180
+RIGHT=0
+
+class Snake:
+    def __init__(self):
+        self.segments=[]
+        self.create_snake()
+        self.head=self.segments[0]
+
+    def create_snake(self):
+        for position in positions:
+            self.add_segment(position)
+
+
+
+    def add_segment(self,position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+
+
+
+
+
+    def move(self):
+        for seg in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(new_x, new_y)
+        self.head.forward(15)
+    def up(self):
+        if self.head.heading()!= DOWN:
+            self.head.setheading(UP)
+            self.head.forward(10)
+
+
+
+    def down(self):
+        if self.head.heading()!=UP:
+            self.head.setheading(DOWN)
+            self.head.forward(10)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+            self.head.forward(10)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+            self.head.forward(10)
+
+
+
+
